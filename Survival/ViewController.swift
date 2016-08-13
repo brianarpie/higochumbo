ca5cc16d9271a0ae53963cb4e8187808e6545fea
Brian Arpie <brian.arpie@gmail.com>
2016-08-13 13:50:54 -0400

    first commit

--- Survival/ViewController.swift
+++ Survival/ViewController.swift
@@ -7,19 +7,63 @@
 //
 
 import UIKit
+import AVFoundation
+import Foundation
 
 class ViewController: UIViewController {
+    
+    var audioPlayer = AVAudioPlayer()
+    
+    // Play a nice tune for the title screen
+    func BackgroundMusic() {
+        if let path = NSBundle.mainBundle().pathForResource("title_music", ofType: "mp3") {
+            let themeMusic = NSURL(fileURLWithPath: path)
+            print(themeMusic)
+            do {
+                audioPlayer = try AVAudioPlayer(contentsOfURL: themeMusic)
+                audioPlayer.numberOfLoops = -1
+                audioPlayer.prepareToPlay()
+                audioPlayer.play()
+            } catch _ {
+                // don't play any music :(
+            }
+
+        }
+    }
+    
+
+    
+    @IBOutlet var gameSummaryLabel: UILabel!
+    
+    @IBAction func newGameButton(sender: UIButton) {
+        
+    }
+    @IBAction func leaderboardButton(sender: UIButton) {
+        
+    }
+    
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationController?.navigationBarHidden = true
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.navigationController?.navigationBarHidden = false
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        BackgroundMusic()
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
